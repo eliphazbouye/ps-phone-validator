540b4819f250b59c2db9d27a769819bda5ec824b
Samuel BOUYE <stranger1698@archlinux.localdomain>
2021-06-01 08:56:18 +0000

    change module name, update code

---
+++ sm_phone_validator.php
@@ -0,0 +1,111 @@
+<?php
+/**
+* 2007-2021 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author    PrestaShop SA <contact@prestashop.com>
+*  @copyright 2007-2021 PrestaShop SA
+*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class Sm_phone_validator extends Module
+{
+    protected $config_form = false;
+
+    public function __construct()
+    {
+        $this->name = 'sm_phone_validator';
+        $this->tab = 'checkout';
+        $this->version = '1.0.0';
+        $this->author = 'Stranger1698';
+        $this->need_instance = 0;
+
+        /**
+         * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
+         */
+        $this->bootstrap = true;
+
+        parent::__construct();
+        $this->displayName = $this->l('Phone Validator');
+        $this->description = $this->l('Display phone validator');
+
+        $this->confirmUninstall = $this->l(' Do you want to display a confirmation message when the merchant tries to uninstall the module ?');
+
+        $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
+    }
+
+    /**
+     * Don't forget to create update methods if needed:
+     * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update
+     */
+    public function install()
+    {
+        Configuration::updateValue('SM_PHONE_VALIDATOR_LIVE_MODE', false);
+
+        return parent::install() &&
+            $this->registerHook('header') &&
+            $this->registerHook('backOfficeHeader');
+    }
+
+    public function uninstall()
+    {
+        Configuration::deleteByName('SM_PHONE_VALIDATOR_LIVE_MODE');
+
+        return parent::uninstall();
+    }
+
+
+    protected function alterCustomerTable()
+    {
+        Db::getInstance()->execute('ALTER TABLE `'. _DB_PREFIX_.'address` ADD `phone_mobile_val` text');
+        return true;
+    }
+
+
+
+    /**
+    * Add the CSS & JavaScript files you want to be loaded in the BO.
+    */
+    public function hookBackOfficeHeader()
+    {
+        if (Tools::getValue('module_name') == $this->name) {
+            $this->context->controller->addJS($this->_path.'views/js/back.js');
+            $this->context->controller->addCSS($this->_path.'views/css/back.css');
+        }
+    }
+
+    /**
+     * Add the CSS & JavaScript files you want to be added on the FO.
+     */
+    public function hookHeader()
+    {
+        $this->context->controller->addJS($this->_path.'/views/js/intlTelInput.js');
+        $this->context->controller->addJS($this->_path.'/views/js/utils.js');
+        $this->context->controller->addJS($this->_path.'/views/js/front.js');
+        $this->context->controller->addCSS($this->_path.'/views/css/front.css');
+        $this->context->controller->addCSS($this->_path.'/views/css/intlTelInput.css');
+
+        return $this->display(__FILE__, 'script.tpl');
+    }
+
+}
